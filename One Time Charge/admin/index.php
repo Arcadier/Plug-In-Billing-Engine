20d102863825e1edecf7a3440daadddae3b96870
Arcadier <ios@arcadier.com>
2019-07-19 18:32:12 +0800

    Update index.php

--- One Time Charge/admin/index.php
+++ One Time Charge/admin/index.php
@@ -9,37 +9,7 @@ $packageId = getPackageID();
 $licence = new License();
 if ($licence->isValid()) {
     ?>
-<!-- begin header-->
-<link rel="stylesheet" href="css/style.css">
-<!--end header-->
-
-<div class="col-sm-9 main-content">
-	<h2>Facebook App Config</h2>
-    <div id="b" class="cause">
-        <div class="inner">
-		    <input type="text" id="fbid" placeholder="FB App ID">
-			<button class= "btn" id="save-btn">Save</button>
-		</div>
-	</div>
-    <h2>Admin Create a Cause</h2>
-    <div id="b" class="cause">
-        <div class="inner">
-            <input type="text" value="" id="input_field-2">
-            <input type="button" value="Create cause" id="create" class="btn">
-        </div>
-    </div>
-    <div id="causes_saved">
-        <h2>Causes created</h2>
-    </div>
-    <table id="table" border="1" style="max-width: 900px;">
-        <tr>
-            <th> Merchant </th>
-            <th id="cause-column" colspan="1" style="text-align:center;"> Causes Supported </th>
-        </tr>
-    </table>
-</div>
-
-<script type="text/javascript" src="scripts/fbApp.js"></script>
+<!-- plugin index.html -->
 <?php
 } else {
     $location = $baseUrl . '/admin/plugins/' . $packageId . '/subscribe.php';
